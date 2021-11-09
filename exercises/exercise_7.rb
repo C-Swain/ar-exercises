023d1ebaee834b6411633eb5b2d5514efd8d6b64
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-09 13:52:53 -0500

    excersise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+#this is to make sure that a store has all the necessary information to be generated
+puts " Please , input a store name"
+user_input = gets.chomp
+user_store = Store.create(name: user_input)
+puts user_store.valid?
+puts user_store.errors.full_messages
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,17 @@
-class Store < ActiveRecord::Base
+class HasApparel < ActiveModel::Validator
+    def validate(record)
+      unless record.mens_apparel || record.womens_apparel
+        record.errors[:name] << 'must carry mens or womens apparel'
+      end
+    end
+  end
+  
+  class Store < ActiveRecord::Base
+    validates :name, length: { minimum: 3 }
+    has_many :employees
+    validates_associated :employees
+    validates :annual_revenue, numericality: { greater_than: 0 }
+    validates_with HasApparel
+  end
 
-end
+
