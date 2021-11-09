b39d9b1ded1d64843bba52abfe72c1fbd7d8ff71
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-09 12:02:20 -0500

    excersize 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+#updated the name 
+@store1.name = "Eaton Centre"
+# confirmed name is updated 
+puts @store1.name
