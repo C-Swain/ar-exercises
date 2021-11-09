10a442e5bb65ef62ebd769955a808850787c9ab1
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-09 13:02:26 -0500

    exercises 5 and 6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,18 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+#Output the total revenue for the entire company (all stores),
+# using Active Record's .sum calculation method.
+@sum_revenue = "#{Store.sum(:annual_revenue)}"
+
+#calculating the avergage
+@average_revenue = "#{Store.average(:annual_revenue)}"
+
+# Output the number of stores that are generating $1M or more in annual sales.
+#  Hint: Chain together where and size (or count) Active Record methods.
+@stores_over_mil = "#{Store.where("annual_revenue > 1000000").count}"
+
+puts @sum_revenue
+puts @average_revenue
+puts @stores_over_mil
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Kimberly", last_name: "Jackson", hourly_rate: 61)
+@store1.employees.create(first_name: "Busty", last_name: "Catman", hourly_rate: 45)
+@store1.employees.create(first_name: "Buttons", last_name: "FluffyFace", hourly_rate: 46)
+@store1.employees.create(first_name: "Anne", last_name: "Rice", hourly_rate: 65)
+
+@store2.employees.create(first_name: "Colin", last_name: "Robinson", hourly_rate: 199)
+@store2.employees.create(first_name: "Nadja", last_name: "Anti-paxos", hourly_rate: 180)
+@store2.employees.create(first_name: "Lazslo", last_name: "Cravensworth", hourly_rate: 90)
+@store2.employees.create(first_name: "Nandy", last_name: "Relentoso", hourly_rate: 188)
+
+
+
+
+
+
